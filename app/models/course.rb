3e18751c8651214b3795c85e400c55a1529dbb29
uimarshall <uimarshall@gmail.com>
2021-03-26 00:04:35 +0100

    Add validations to database models

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ApplicationRecord
     has_many :measurements, dependent: :destroy
     has_many :enrolments
     has_many :users, through: :enrolments
+
+    validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,21 @@ class User < ApplicationRecord
   has_secure_password
   has_many :enrolments
   has_many :courses, through: :enrolments
+
+  before_save :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates_presence_of :name, presence: true,
+            length: { minimum: 2 },
+            uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 7 }                  
+
+
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
