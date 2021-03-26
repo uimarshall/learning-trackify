c18f6c66487a134f033a18b41b2d8ac128d4c387
uimarshall <uimarshall@gmail.com>
2021-03-26 23:12:52 +0100

    make minor changes for bug fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,15 +2,41 @@ module Api
   module V1
     class UsersController < ApplicationController
       skip_before_action :authorize_request, only: :create
+      skip_before_action :verify_authenticity_token, only: :create
+
+
+#       def create
+#     user = User.create!(user_params)
+#     auth_token = AuthenticateUser.new(user.email, user.password).call
+#     response = { message: Message.account_created, auth_token: auth_token }
+#     json_response(response, :created)
+#   end
+
+#   private
+
+#   def user_params
+#     params.permit(
+#       :name,
+#       :email,
+#       :password,
+#       :password_confirmation
+#     )
+#   end
+
+
+
+
+
 
       def create
         user = User.new(user_params)
-        if new_user.save
+        if user.save
           # If a user is created, get us a token
           auth_token = AuthenticateUser.new(user.email, user.password).call
           time = Time.now + 24.hours.to_i
           response = { message: Message.account_created, auth_token: auth_token, time: time }
-          render json: UserSerializer.new(response).serialized_json
+        #   render json: UserSerializer.new(response).serialized_json
+          render json: response, status: :ok
         else
           head(:unprocessable_entity)
         end
