4a5cbe4756a2ed6ab3f3b04f0751f01e50f77cdb
uimarshall <uimarshall@gmail.com>
2021-03-28 22:09:12 +0100

    Add measurement reducers

---
+++ app/javascript/reducers/measurement.js
@@ -0,0 +1,18 @@
+import {
+   MY_MEASUREMENTS_OK, MY_MEASUREMENTS_BAD, MY_MEASUREMENTS_LOADING
+  
+} from '../actions/actionTypes';
+
+const measurementReducer = (measurement = {}, { type, payload }) => {
+  switch (type) {
+    case MY_MEASUREMENTS_OK:
+      return { ...measurement, myMeasurements: payload, myLoading: false };
+    case MY_MEASUREMENTS_BAD:
+      return { ...measurement, myMessage: payload.message, myLoading: false };
+    case MY_MEASUREMENTS_LOADING:
+      return { ...measurement, myLoading: true };
+    default:
+      return measurement;
+  }
+};
+export default measurementReducer;
