f85dfa3ff2e068a00f55ac15b4e4d16c52e816c9
uimarshall <uimarshall@gmail.com>
2021-03-28 23:01:39 +0100

    modified react index page

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,25 +2,49 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+// const Hello = props => (
+//   <div>Hello {props.name}!</div>
+// )
 
-Hello.defaultProps = {
-  name: 'David'
-}
+// Hello.defaultProps = {
+//   name: 'David'
+// }
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
+// Hello.propTypes = {
+//   name: PropTypes.string
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(
+//     <Hello name="React" />,
+//     document.body.appendChild(document.createElement('div')),
+//   )
+// })
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import App from '../components/App';
+import rootReducer from '../reducers';
+// import '../assets/scss/style.scss';
+// uncomment this line if you cant see styles in dev.
+import defaultProps from '../data/defaultProps';
+
+const store = createStore(rootReducer, defaultProps, applyMiddleware(thunk));
+
+const wrappedApp = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+  ReactDOM.render(wrappedApp, document.body.appendChild(document.createElement('div')));
+});
